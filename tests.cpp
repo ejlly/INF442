ca88402bd7f85b3f7a2fde1b651859e3510e7773
ejlly <ejllythecrafter@gmail.com>
2022-05-16 20:44:00 +0200

    add option parsing for undirected graphs

--- tests.cpp
+++ tests.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <ctime>
+#include <cstring>
+#include <algorithm>
 
 #include "tarjan.hpp"
 
 int main(int argc, char **argv){
 
-	if(argc == 1){
-		std::cout << "Usage : " << argv[0] << " filename1 filename2 ...\n";
+	if(argc < 3){
+		std::cout << "Usage : " << argv[0] << " [-d|-u] filename1 filename2 ...\n";
 		return 0;
 	}
 
 	//Set rand seed for ER-graphs
 	std::srand(time(nullptr));
 	
+	//https://stackoverflow.com/questions/865668/parsing-command-line-arguments-in-c
+	bool directed;
+
+	if(std::strcmp(argv[1], "-d")){
+		directed = true;
+	}
+	else if(std::strcmp(argv[1], "-u")){
+		directed = false;
+	}
+	else{
+		std::cout << "Usage : " << argv[0] << " [-d|-u] filename1 filename2 ...\n";
+		return 0;
+	}
+
+
 	//Read .edges files
-	for(int i(1); i<argc; i++){
-		Graph g(argv[i]);
+	for(int i(2); i<argc; i++){
+		Graph g(argv[i], directed);
 		//g.print();
 
 		std::vector<std::vector<int>> test = tarjan(g);
