e600ce2fb0dc23471aa232f5365b5e4c9f826561
Fabien Roger <efab.roger@gmail.com>
2022-05-16 22:10:22 +0200

    Do all ER experiments

--- plotter.py
+++ plotter.py
@@ -8,13 +8,18 @@ plt.xscale("log")
 plt.show()
 
 # For ER prob experiment
-df = pd.read_csv("experiments_results/ER_prob.csv", index_col=None)
-df.plot(
-    x="p",
-    y=["avg_nb_composantes", "avg_nb_composantes_isolees"],
-    label=["number of components", "number of isolated components"],
-    marker="o",
-    color=["r", "b"],
-)
-plt.yscale("log")
-plt.show()
+
+sizes = [25,100,1000]
+file_names = [f"experiments_results/ER_prob_{s}.csv" for s in sizes]
+
+for file_name in file_names:
+    df = pd.read_csv(file_name, index_col=None)
+    df.plot(
+        x="p",
+        y=["avg_nb_composantes", "avg_nb_composantes_isolees"],
+        label=["number of components", "number of isolated components"],
+        marker="o",
+        color=["r", "b"],
+    )
+    plt.yscale("log")
+    plt.show()
