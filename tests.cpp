c220dfed4dc0f5b76239cd24709440755a253c5c
ejlly <ejllythecrafter@gmail.com>
2022-05-16 19:37:44 +0200

    add tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
+#include <ctime>
 
-#include "dbscan.hpp"
-#include "graph.hpp"
-#include "twoDGraph.hpp"
+#include "tarjan.hpp"
 
-int main() {
-    std::cout << "Hello world" << std::endl;
-    Graph g(2, 0.5);
-    g.print();
-    Graph g2(5, 0.5);
-    g2.print();
-    Graph g3(5, 0.1);
-    g3.print();
+int main(int argc, char **argv){
 
-    naive_dbscan(g, DbscanParameters(1.5, 1));
-    for (int c : g.clusters) std::cout << c << " ";
-    std::cout << std::endl;
-    naive_dbscan(g2, DbscanParameters(1.5, 0));
-    for (int c : g.clusters) std::cout << c << " ";
-    std::cout << std::endl;
-    naive_dbscan(g3, DbscanParameters(1.5, 0));
-    for (int c : g.clusters) std::cout << c << " ";
-    std::cout << std::endl;
+	if(argc == 1){
+		std::cout << "Usage : " << argv[0] << " filename1 filename2 ...\n";
+		return 0;
+	}
 
-    TwoDGraph g2D(30);
-    naive_dbscan(g2D, DbscanParameters(0.1, 3));
-    g2D.draw(40);
+	//Set rand seed for ER-graphs
+	std::srand(time(nullptr));
+	
+	//Read .edges files
+	for(int i(1); i<argc; i++){
+		Graph g(argv[i]);
+		//g.print();
+
+		std::vector<std::vector<int>> test = tarjan(g);
+
+		aff_cnx(test);
+	}
+
 }
