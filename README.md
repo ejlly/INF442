d1943d2cbd153b9aa34522b0e791cc5ca3fe9d4a
Fabien Roger <efab.roger@gmail.com>
2022-05-17 23:25:23 +0200

    Complete the readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# INF442
-Le projet 5 d'INF442
+# Experiments With Connected Components, ER Graphs and DBSCAN
+
+## Results and methodology
+
+See `report.pdf`
+
+## How to run
+
+To run, compile all the `.cpp` files and run the resulting executable with one of the following flags:
+
+- For dbscan experiments: `[-d|-u] filename1 filename2 ...` (use `-u` for undirected graph, if the edges are not already duplicated, and `-d` for directed graph, or if the edges are already duplicated)
+- For ER experiments: `-e N NB_TEST NB_PROB MAX_VALUE path`
+- For experiments on 2D plane graphs with given M & eps: `-p M EPS NB_TEST INCR_N MAX_N path`
+- For experiments on 2D plane graphs with an heuristics to find M & eps: `-h NB_TEST INCR_N MAX_N path`
+
+To create the plots, you need to use Python 3 with pandas and matplotlib. Change the file names inside the `plotter.py` file so that it matches yours, and then run `python plotter.py`
--- plotter.py
+++ plotter.py
@@ -1,33 +1,33 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# For facebook dbscan experiment
-# df = pd.read_csv("experiments_results/facebook_dbscan.csv", index_col=None)
-# df.plot(x="M", y="max_cluster_size", marker="o")
-# plt.xscale("log")
-# plt.show()
+# For dbscan experiment
+
+df = pd.read_csv("experiments_results/facebook_dbscan.csv", index_col=None)
+df.plot(x="M", y="max_cluster_size", marker="o")
+plt.xscale("log")
+plt.show()
 
 # For ER prob experiment
 
-# sizes = [25, 100, 1000]
-# file_names = [f"experiments_results/ER_prob_{s}.csv" for s in sizes]
+sizes = [25, 100, 1000]
+file_names = [f"experiments_results/ER_prob_{s}.csv" for s in sizes]
 
-# for file_name in file_names:
-#     df = pd.read_csv(file_name, index_col=None)
-#     df.plot(
-#         x="p",
-#         y=["avg_nb_composantes", "avg_nb_composantes_isolees"],
-#         label=["number of components", "number of isolated components"],
-#         marker="o",
-#         color=["r", "b"],
-#     )
-#     plt.yscale("log")
-#     plt.show()
+for file_name in file_names:
+    df = pd.read_csv(file_name, index_col=None)
+    df.plot(
+        x="p",
+        y=["avg_nb_composantes", "avg_nb_composantes_isolees"],
+        label=["number of components", "number of isolated components"],
+        marker="o",
+        color=["r", "b"],
+    )
+    plt.yscale("log")
+    plt.show()
 
 # For 2D experiment
 
-# params = ["4_0.2", "4_0.1", "4_0.05", "8_0.1", "16_0.1", "64_0.1", "heuristics"]
-params = ["heuristics"]
+params = ["4_0.2", "4_0.1", "4_0.05", "8_0.1", "16_0.1", "64_0.1", "heuristics"]
 file_names = [f"experiments_results/2D_{p}.csv" for p in params]
 
 for file_name in file_names:
